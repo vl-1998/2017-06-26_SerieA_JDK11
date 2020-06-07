12a1ee7b33c51c90d20080be75adcbbc31d0a11d
s244926 <s244926@studenti.polito.it>
2020-06-08 00:26:29 +0200

    Progetto Base turno B

--- src/main/java/it/polito/tdp/seriea/FXMLController.java
+++ src/main/java/it/polito/tdp/seriea/FXMLController.java
@@ -16,7 +16,7 @@ public class FXMLController {
 	
 	private Model model;
 
-    @FXML
+	@FXML
     private ResourceBundle resources;
 
     @FXML
@@ -26,42 +26,38 @@ public class FXMLController {
     private ChoiceBox<?> boxSquadra;
 
     @FXML
-    private ChoiceBox<?> boxStagione;
-
-    @FXML
     private Button btnCalcolaConnessioniSquadra;
 
     @FXML
-    private Button btnSimulaTifosi;
+    private Button btnSimulaEspulsi;
 
     @FXML
-    private Button btnAnalizzaSquadre;
+    private Button btnAnalizzaStagioni;
 
     @FXML
     private TextArea txtResult;
 
     @FXML
-    void doAnalizzaSquadre(ActionEvent event) {
+    void doAnalizzaStagioni(ActionEvent event) {
 
     }
 
     @FXML
-    void doCalcolaConnessioniSquadra(ActionEvent event) {
+    void doCalcolaConnessioniStagione(ActionEvent event) {
 
     }
 
     @FXML
-    void doSimulaTifosi(ActionEvent event) {
+    void doSimulaEspulsi(ActionEvent event) {
 
     }
 
     @FXML
     void initialize() {
         assert boxSquadra != null : "fx:id=\"boxSquadra\" was not injected: check your FXML file 'SerieA.fxml'.";
-        assert boxStagione != null : "fx:id=\"boxStagione\" was not injected: check your FXML file 'SerieA.fxml'.";
         assert btnCalcolaConnessioniSquadra != null : "fx:id=\"btnCalcolaConnessioniSquadra\" was not injected: check your FXML file 'SerieA.fxml'.";
-        assert btnSimulaTifosi != null : "fx:id=\"btnSimulaTifosi\" was not injected: check your FXML file 'SerieA.fxml'.";
-        assert btnAnalizzaSquadre != null : "fx:id=\"btnAnalizzaSquadre\" was not injected: check your FXML file 'SerieA.fxml'.";
+        assert btnSimulaEspulsi != null : "fx:id=\"btnSimulaEspulsi\" was not injected: check your FXML file 'SerieA.fxml'.";
+        assert btnAnalizzaStagioni != null : "fx:id=\"btnAnalizzaStagioni\" was not injected: check your FXML file 'SerieA.fxml'.";
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'SerieA.fxml'.";
 
     }
