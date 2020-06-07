32b767ed09d43e89dbf8f1aacdc5d9d0bb8ec5a0
s244926 <s244926@studenti.polito.it>
2020-06-08 00:23:20 +0200

    Progetto Base turno A

---
+++ src/main/java/it/polito/tdp/seriea/FXMLController.java
@@ -0,0 +1,72 @@
+package it.polito.tdp.seriea;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.seriea.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+
+//controller turno A --> switchare al branch master_turnoB o master_turnoC per turno B o C
+
+public class FXMLController {
+	
+	private Model model;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ChoiceBox<?> boxSquadra;
+
+    @FXML
+    private ChoiceBox<?> boxStagione;
+
+    @FXML
+    private Button btnCalcolaConnessioniSquadra;
+
+    @FXML
+    private Button btnSimulaTifosi;
+
+    @FXML
+    private Button btnAnalizzaSquadre;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    void doAnalizzaSquadre(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doCalcolaConnessioniSquadra(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doSimulaTifosi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert boxSquadra != null : "fx:id=\"boxSquadra\" was not injected: check your FXML file 'SerieA.fxml'.";
+        assert boxStagione != null : "fx:id=\"boxStagione\" was not injected: check your FXML file 'SerieA.fxml'.";
+        assert btnCalcolaConnessioniSquadra != null : "fx:id=\"btnCalcolaConnessioniSquadra\" was not injected: check your FXML file 'SerieA.fxml'.";
+        assert btnSimulaTifosi != null : "fx:id=\"btnSimulaTifosi\" was not injected: check your FXML file 'SerieA.fxml'.";
+        assert btnAnalizzaSquadre != null : "fx:id=\"btnAnalizzaSquadre\" was not injected: check your FXML file 'SerieA.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'SerieA.fxml'.";
+
+    }
+
+	public void setModel(Model model) {
+		this.model = model;
+	}
+}
